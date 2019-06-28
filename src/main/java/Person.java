5c79335924af5849d151f89deab3d3c3a0fb8f94
Robin SAINT GEORGES <robin.sgeorges@gmail.com>
2019-06-28 16:42:08 +0200

    init

---
+++ src/main/java/Person.java
@@ -0,0 +1,19 @@
+public class Person {
+    public String getname(){
+        return "";
+    }
+    public String getTelNum(){
+        return "";
+    }
+    private int getOfficeAreaCode(){
+        return 14;
+    }
+    private void setOfficeAreaCode(int code){
+
+    }
+    private int getNumber(){
+        return 14;
+    }
+    private void setNumber(){
+    }
+}
